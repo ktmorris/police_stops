c09b747c9677761f9b26c07a03aadf5c8f368164
Kevin <kevin.morris@nyu.edu>
2021-05-10 14:13:52 -0400

    local stuff

--- code/hills/mayor/04_mayor_reg.R
+++ code/hills/mayor/04_mayor_reg.R
@@ -48,44 +48,10 @@ matches <- left_join(matches,
 
 cleanup("matches")
 gc()
-ll <- matches %>%
-  filter(first_tr_year - last_date <= 90, period <= 0.5, period >= -1.5) %>%
-  group_by(treated, period, black_t) %>%
-  summarize(to = weighted.mean(to, weight)) %>% 
-  mutate(treated = ifelse(treated, "Treated", "Control"),
-         black = ifelse(black_t, "Black Voters", "Non-Black Voters"))
-
-
-ll$treated <- factor(ll$treated, levels = c("Treated", "Control"))
-
-p1 <- ggplot(data = ll) + 
-  facet_grid(. ~ black) +
-  geom_rect(aes(xmin = 0.5-.125, xmax = 0.5, ymin = 0, ymax = Inf),
-            alpha = 0.03, color = "black", fill = "yellow") +
-  geom_line(data =ll, aes(x = period, y = to, linetype = treated)) +
-  geom_point(data = ll, aes(x = period, y = to, shape = treated)) +
-  scale_x_continuous(minor_breaks = seq(-3.5, 3.5, 1),
-                     breaks = seq(-3.5, 3.5, 1),
-                     labels = c("-4",
-                                "-3",
-                                "-2",
-                                "-1",
-                                "0",
-                                "1",
-                                "2",
-                                "3")) +
-  theme_bc(base_family = "LM Roman 10") +
-  scale_y_continuous(labels = percent) +
-  labs(x = "t", y = "Turnout",
-       linetype = "Treatment Group",
-       shape = "Treatment Group",
-       caption = "Treatment occurs inside of yellow band.") 
-p1
-saveRDS(p1, "temp/within90days_y.rds")
 ##########################
 ll <- matches %>%
   filter(period <= .5, period >= -1.5) %>% 
-  group_by(treated, period, black_t, first_tr_year) %>%
+  group_by(treated, period, black_t) %>%
   summarize(to = weighted.mean(to, weight)) %>% 
   mutate(treated = ifelse(treated, "Treated", "Control"),
          black = ifelse(black_t, "Black Voters", "Non-Black Voters"))
@@ -93,7 +59,7 @@ ll <- matches %>%
 ll$treated <- factor(ll$treated, levels = c("Treated", "Control"))
 
 p2 <- ggplot(data = ll) + 
-  facet_grid(first_tr_year ~ black) +
+  facet_grid( ~ black) +
   geom_rect(aes(xmin = -.49, xmax = 0.5, ymin = 0, ymax = Inf),
             alpha = 0.03, color = "black", fill = "yellow") +
   geom_line(data =ll, aes(x = period, y = to, linetype = treated)) +
@@ -115,4 +81,76 @@ p2 <- ggplot(data = ll) +
        shape = "Treatment Group",
        caption = "Treatment occurs inside of yellow band.")
 p2
+ggsave(plot = p2, file = "markdown/local_fig.png")
 #####################
+
+dat1 <- filter(matches, period <= 0.5)
+
+m1 <- to ~ treated * post + as.factor(year)
+
+m2 <- to ~ treated * post + as.factor(year) +
+  white + black + latino + asian + male +
+  dem + rep + age + reg_date + pre_stops + v1 + v2 +
+  median_income + some_college + unem + civil + paid + tampa_pd
+
+m3 <- to ~ treated * post * black + as.factor(year)
+
+m4 <- to ~ treated * post * black + as.factor(year) +
+  white + black + latino + asian + male +
+  dem + rep + age + reg_date + pre_stops + v1 + v2 +
+  median_income + some_college + unem + civil + paid + tampa_pd
+
+models1 <- lapply(c(m1, m2, m3, m4), function(f){
+  m <- lm(f, data = dat1,
+          weight = dat1$weight)
+})
+
+
+ses_cl <- list(
+  summary(lm.cluster(formula = m1, data = dat1, weights = dat1$weight, cluster = dat1$group))[ , 2],
+  summary(lm.cluster(formula = m2, data = dat1, weights = dat1$weight, cluster = dat1$group))[ , 2],
+  summary(lm.cluster(formula = m3, data = dat1, weights = dat1$weight, cluster = dat1$group))[ , 2],
+  summary(lm.cluster(formula = m4, data = dat1, weights = dat1$weight, cluster = dat1$group))[ , 2]
+)
+
+
+stargazer(models1,
+          type = "text",
+          omit.stat = c("f", "ser"),
+          se = ses_cl,
+          omit = c("as.fac"),
+          covariate.labels = c("Treated",
+                               "Post Treatment",
+                               "Treated $\\times$ Post Treatment",
+                               "Black",
+                               "Treated $\\times$ Black",
+                               "Post Treatment $\\times$ Black",
+                               "Treated $\\times$ Post Treatment $\\times$ Black"),
+          table.layout = "-cm#-t-a-s-n",
+          keep = c("treatedTRUE", "post", "blackTRUE", "Constant"),
+          order = c(1, 2, 4, 22, 23, 21, 24),
+          notes = "TO REPLACE",
+          table.placement = "H",
+          title = "\\label{tab:dind-table} Turnout Effects of Tickets",
+          out = "temp/two_matches_reg_y_loc.tex",
+          add.lines = list(c("Includes Matched Covariates", "", "X", "", "X"),
+                           c("Includes Year Fixed Effects", "X", "X", "X", "X")))
+
+j <- fread("./temp/two_matches_reg_y_loc.tex", header = F, sep = "+")
+
+note.latex <- "\\multicolumn{5}{l}{\\scriptsize{\\parbox{.5\\linewidth}{\\vspace{2pt}$^{***}p<0.01$, $^{**}p<0.05$, $^*p<0.1$. \\\\Robust standard errors (clustered at level of match) in parentheses.}}}"
+
+j <- j %>% 
+  mutate(n = row_number(),
+         V1 = ifelse(grepl("TO REPLACE", V1), note.latex, V1),
+         V1 = ifelse(grepl("\\\\#tab", V1), gsub("\\\\#", "", V1), V1)) %>% 
+  filter(!grepl("Note:", V1))
+
+
+j <- j %>%
+  arrange(n) %>%
+  select(-n)
+
+
+write.table(j, "./temp/dind_reg_y_loc.tex", quote = F, col.names = F,
+            row.names = F)
